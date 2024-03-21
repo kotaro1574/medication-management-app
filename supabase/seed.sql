8dce4cad8598496ddc8d0809ec5971729ac78f12
kotaro1574 <shake1574@gmail.com>
2024-03-21 14:27:10 -0700

    supabase/seed.sqlの変更: データの入れ替え

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,4 +1,4 @@
 insert into public.patients (name, image_id, face_ids)
 values
-('ktraw1574', 'ece685b8-58d2-4bf5-848a-3ada030e39a8', array['ab726641-0580-40b3-b9d0-1a534549ac5a']),
-('Ollie.', '8d50170a-c640-493d-8621-ff57df74dd5d', array['d8004bed-9f63-45d5-91f2-e9d2c29c0f8f']);
+('ktraw1574', 'ece685b8-58d2-4bf5-848a-3ada030e39a8', array['d8004bed-9f63-45d5-91f2-e9d2c29c0f8f']),
+('Ollie.', '8d50170a-c640-493d-8621-ff57df74dd5d', array['ab726641-0580-40b3-b9d0-1a534549ac5a']);
