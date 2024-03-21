2bd873e8316e3db49e9c967b4d73f925714f4884
kotaro1574 <shake1574@gmail.com>
2024-03-20 17:45:06 -0700

    患者テーブルにimage_idを追加するスクリプトとseedデータの変更を追加

---
+++ supabase/migrations/20240321003704_add_imageid_to_patients_table.sql
@@ -0,0 +1,2 @@
+alter table
+if exists public.patients add image_id text;
--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,5 +1,4 @@
-insert into
-public.patients (name)
-values
-('ktraw1574'),
-('Ollie.');
+INSERT INTO public.patients (name, image_id)
+VALUES
+('ktraw1574', 'ece685b8-58d2-4bf5-848a-3ada030e39a8'),
+('Ollie.', '8d50170a-c640-493d-8621-ff57df74dd5d');
