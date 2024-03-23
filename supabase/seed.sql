6cb4309ffeae76e2c40992537fd533ff9859a3c8
kotaro1574 <shake1574@gmail.com>
2024-03-23 10:58:03 -0700

    update: seedにdata追加

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,4 +1,5 @@
 insert into public.patients (name, image_id, face_ids)
 values
 ('ktraw1574', 'ece685b8-58d2-4bf5-848a-3ada030e39a8', array['d8004bed-9f63-45d5-91f2-e9d2c29c0f8f']),
-('Ollie.', '8d50170a-c640-493d-8621-ff57df74dd5d', array['ab726641-0580-40b3-b9d0-1a534549ac5a']);
+('Ollie.', '8d50170a-c640-493d-8621-ff57df74dd5d', array['ab726641-0580-40b3-b9d0-1a534549ac5a']),
+('Totori', 'd9dc2a7a-11c7-4a7f-bd68-153bf207fae9', array['20aa2eb0-0c30-4a10-8fa6-313e4d0595c1']);
