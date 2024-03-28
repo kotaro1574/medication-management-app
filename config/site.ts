87180ae1eff7a6a095d9dcc7a567a00e99661400
kotaro1574 <shake1574@gmail.com>
2024-03-28 14:24:20 -0700

    delete: 不要なコードを削除

--- config/site.ts
+++ config/site.ts
@@ -18,9 +18,4 @@ export const siteConfig = {
       href: "/ocr",
     },
   ],
-  links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
-  },
 }
