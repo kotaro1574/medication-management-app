b2eec49946b5e5450838ce96c6f35e73dbc790f8
kotaro1574 <shake1574@gmail.com>
2024-03-29 14:20:08 -0700

    update: OCRはwebcam使わない

--- app/ocr/azure-computer-vision/page.tsx
+++ app/ocr/azure-computer-vision/page.tsx
@@ -1,21 +1,37 @@
 "use client"
 
-import { useCallback, useRef, useState } from "react"
-import Webcam from "react-webcam"
+import { useState } from "react"
+import Image from "next/image"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
+import { z } from "zod"
 
-import { Button } from "@/components/ui/button"
+import { toBase64 } from "@/lib/utils"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { Form, FormItem, FormLabel } from "@/components/ui/form"
+
+const formSchema = z.object({
+  imageFile: z.custom<File>().nullable(),
+})
 
 export default function AzureComputerVisionPage() {
   const [loading, setLoading] = useState(false)
   const [message, setMessage] = useState("")
-  const webcamRef = useRef<Webcam>(null)
 
-  const onOCR = useCallback(async () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      imageFile: null,
+    },
+    resolver: zodResolver(formSchema),
+  })
+
+  const onSubmit = async ({ imageFile }: z.infer<typeof formSchema>) => {
     try {
       setLoading(true)
-      const base64_image =
-        webcamRef.current?.getScreenshot()?.split(",")[1] ?? ""
-      if (!base64_image) return
+
+      if (!imageFile) return
+
+      const base64_image = await toBase64(imageFile)
 
       const response = await fetch("/api/azure-computer-vision", {
         method: "POST",
@@ -25,15 +41,15 @@ export default function AzureComputerVisionPage() {
         body: JSON.stringify({ image: base64_image }),
       })
 
-      const data = await response.json()
+      const result = await response.json()
 
-      setMessage(data.response)
+      setMessage(result.response)
     } catch (error) {
       console.error(error)
     } finally {
       setLoading(false)
     }
-  }, [webcamRef])
+  }
 
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
@@ -46,27 +62,63 @@ export default function AzureComputerVisionPage() {
         </p>
       </div>
 
-      <div>
-        {!message ? (
-          <>
-            <Webcam
-              className="rounded-md"
-              audio={false}
-              ref={webcamRef}
-              screenshotFormat="image/jpeg"
-              videoConstraints={{ facingMode: "environment" }}
-            />
-            <Button disabled={loading} className="mt-4" onClick={onOCR}>
-              {loading ? "Loading..." : "テキストを抽出"}
-            </Button>
-          </>
-        ) : (
-          <>
-            <p className="text-xl font-bold">抽出されたテキスト👇</p>
-            <p className="mt-4 max-w-[700px] text-2xl">{message}</p>
-          </>
-        )}
-      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <Controller
+            render={({ field: { onChange, value } }) => (
+              <FormItem>
+                <FormLabel htmlFor="imageFile">Image</FormLabel>
+                {value && (
+                  <Image
+                    src={URL.createObjectURL(value)}
+                    width={300}
+                    alt="selected_image"
+                    height={300}
+                  />
+                )}
+                <div className="relative">
+                  <label
+                    className={`${buttonVariants({
+                      variant: "default",
+                      size: "default",
+                    })} mt-2`}
+                    htmlFor="single"
+                  >
+                    {form.getValues("imageFile") ? "画像を変更" : "画像を選ぶ"}
+                  </label>
+
+                  <input
+                    style={{
+                      visibility: "hidden",
+                      position: "absolute",
+                      width: 0,
+                    }}
+                    type="file"
+                    id="single"
+                    accept="image/*"
+                    onChange={(e) => {
+                      if (!e.target.files?.[0]) return
+                      onChange(e.target.files?.[0] as File)
+                    }}
+                    disabled={loading}
+                  />
+                </div>
+              </FormItem>
+            )}
+            name="imageFile"
+            control={form.control}
+          />
+          <Button disabled={loading} type="submit">
+            {loading ? "loading..." : "OCRする"}
+          </Button>
+        </form>
+      </Form>
+      {message && (
+        <>
+          <p className="text-xl font-bold">抽出されたテキスト👇</p>
+          <p className="mt-4 max-w-[700px] text-2xl">{message}</p>
+        </>
+      )}
     </section>
   )
 }
