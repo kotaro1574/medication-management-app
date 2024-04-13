b2a37744f52fb802b300be8dc8d6ed40d421f9e4
kotaro1574 <shake1574@gmail.com>
2024-04-13 11:23:48 -0700

    update: リンク追加

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 import { SignUpForm } from "./sign-up-form"
 
 export default function SignUpPage() {
@@ -5,6 +7,9 @@ export default function SignUpPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="max-w-[350px]">
         <SignUpForm />
+      </div>
+      <div>
+        <Link href="/login">アカウントをお持ちの方はこちら</Link>
       </div>
     </section>
   )
