4ebe76d3dcf80831c6d1acbe0155ad8360e1cdd1
kotaro1574 <shake1574@gmail.com>
2024-04-19 12:47:48 -0700

    create: Patientsページ作成

---
+++ app/patients/page.tsx
@@ -0,0 +1,7 @@
+export default async function PatientsPage() {
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <h1>Patients</h1>
+    </section>
+  )
+}
