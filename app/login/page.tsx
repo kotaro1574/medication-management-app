ca2268808edc5ecd525648ddd8ae0c122be59621
kotaro1574 <shake1574@gmail.com>
2024-05-05 13:29:49 -0700

    fix: loginページのstyle調整

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ import { LoginForm } from "@/feature/auth/login-form"
 
 export default function LoginPage() {
   return (
-    <div className="px-[60px] py-[120px]">
+    <div className="container max-w-[450px] py-[120px]">
       <h1 className="text-center text-[24px] font-bold text-[#c2b37f]">
         ログイン
       </h1>
