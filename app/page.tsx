39ef566c42a0a7aee13ee790b31d013a4e708cc4
kotaro1574 <shake1574@gmail.com>
2024-05-11 14:47:11 -0700

    update: リンクをHeaderに移動

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
-import Link from "next/link"
-
 export default async function IndexPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div>
-        <Link href="/sign-up">Sign Up</Link>
-      </div>
-      <div>
-        <Link href="/login">Login</Link>
-      </div>
-      <div>
-        <Link href="/patients">Patients</Link>
-      </div>
-      <div>
-        <Link href="/recognition">Recognition</Link>
-      </div>
-      <div>
-        <Link href="/ocr">OCR</Link>
-      </div>
-    </section>
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10"></section>
   )
 }
