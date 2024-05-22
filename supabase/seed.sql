d8961ac010b6bff6375bc2bdafae112222aadddd
kotaro1574 <shake1574@gmail.com>
2024-05-22 13:05:44 -0700

    update: seed

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -3,3 +3,9 @@ values
 ('ktraw1574', '5f9e89a6-180f-4343-aa9c-e66af3a4c8d1', array['6c464c39-a82d-4c8a-9610-4f0dde97e845']),
 ('Ollie.', '4301dd77-f600-45b7-af6e-96a1c3188cec', array['3cc67684-3799-432b-b9a2-077e75737672']),
 ('Totori', '1cdddf84-6dd2-4dc0-800f-65623f07c067', array['75c3a9fe-d3cc-466e-bb82-b449d537c9b4']);
+
+INSERT INTO facilities (name) VALUES
+('健康センターA'),
+('リハビリテーション施設B'),
+('メディカルプラザ'),
+('フィットネスクリニック');
