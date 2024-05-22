81da63c1435e9c61d2685570fd72003ee9cbdbc2
kotaro1574 <shake1574@gmail.com>
2024-05-22 13:47:49 -0700

    create: facilities-select作成

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,6 +1,6 @@
 import { SignUpForm } from "@/feature/auth/sign-up-form"
 
-export default function SignUpPage() {
+export default async function SignUpPage() {
   return (
     <div className="container max-w-[450px] py-[120px]">
       <h1 className="text-center text-[24px] font-bold text-[#c2b37f]">
