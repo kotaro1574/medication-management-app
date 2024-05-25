0036ad95bf6d852cee16a8402a5665f35fce5781
kotaro1574 <shake1574@gmail.com>
2024-05-25 10:42:16 -0700

    update: bucketを受け取れるようにした

--- lib/aws/utils.ts
+++ lib/aws/utils.ts
@@ -6,11 +6,11 @@ import { v4 as uuidv4 } from "uuid"
 import { rekognitionClient, s3Client } from "@/lib/aws/aws-clients"
 
 // Rekognitionコレクションから顔データを削除する関数
-export async function deleteFace(collectionId: string, faceIds: string[]) {
+export async function deleteFace(bucket: string, faceIds: string[]) {
   try {
     await rekognitionClient.send(
       new DeleteFacesCommand({
-        CollectionId: collectionId,
+        CollectionId: bucket,
         FaceIds: faceIds,
       })
     )
@@ -20,10 +20,10 @@ export async function deleteFace(collectionId: string, faceIds: string[]) {
 }
 
 // S3から画像を削除する関数
-export async function deleteImage(key: string) {
+export async function deleteImage(key: string, bucket: string) {
   try {
     const deleteParams = {
-      Bucket: process.env.AMPLIFY_BUCKET,
+      Bucket: bucket,
       Key: key,
     }
     await s3Client.send(new DeleteObjectCommand(deleteParams))
@@ -33,10 +33,10 @@ export async function deleteImage(key: string) {
 }
 
 // 画像をS3にアップロードするためのプリサインドURLを取得する関数
-export async function getPresignedUrl(fileType: string) {
+export async function getPresignedUrl(fileType: string, bucket: string) {
   const key = uuidv4()
   const { url, fields } = await createPresignedPost(s3Client, {
-    Bucket: process.env.AMPLIFY_BUCKET ?? "",
+    Bucket: bucket,
     Key: key,
     Conditions: [
       ["content-length-range", 0, 104857600], // 最大10MB
