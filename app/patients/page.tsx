9234701577efda6d57bae93458acebf69375439d
nyny-sugar <nyny.sugar@gmail.com>
2024-06-06 14:04:46 -0700

    スタイル調整

--- app/patients/page.tsx
+++ app/patients/page.tsx
@@ -1,28 +1,44 @@
+"use client"
+
 import Link from "next/link"
 
 import { createClient } from "@/lib/supabase/server"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
-export default async function PatientsPage() {
-  const supabase = createClient()
-  const { data: patients } = await supabase.from("patients").select("*")
-
+export default function PatientsPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="mx-auto">
         <h2 className="">4/1(水)</h2>
-        
+        <Tabs defaultValue="account" className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="all">全て</TabsTrigger>
+            <TabsTrigger value="floorA">Aフロア</TabsTrigger>
+            <TabsTrigger value="floorB">Bフロア</TabsTrigger>
+            <TabsTrigger value="floorC">Cフロア</TabsTrigger>
+            <TabsTrigger value="floorD">Dフロア</TabsTrigger>
+            <TabsTrigger value="floorE">Eフロア</TabsTrigger>
+          </TabsList>
+          <TabsContent value="all">
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value="floorA">
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value="floorB">
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value="floorC">
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value="floorD">
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value="floorE">
+            Make changes to your account here.
+          </TabsContent>
+        </Tabs>
       </div>
-
-      <ul>
-        {patients?.map((patient) => (
-          <li key={patient.id}>
-            <Link href={`/patients/${patient.id}`}>
-              {patient.last_name} {patient.first_name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Link href="/patients/create">Create Patient</Link>
     </section>
   )
 }
