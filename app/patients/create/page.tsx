88c9c2c4be931db374f20d732f9b4827c3f383d5
kotaro1574 <shake1574@gmail.com>
2024-06-07 12:45:24 -0700

    update: 利用者情報Formとりあえず表示

--- app/patients/create/page.tsx
+++ app/patients/create/page.tsx
@@ -1,13 +1,224 @@
-import { CreatePatientForm } from "@/feature/patient/create-patient-form"
+"use client"
+
+import { GroupsSelect } from "@/feature/group/groups-select"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+const formSchema = z.object({
+  faceImages: z
+    .array(z.custom<File>())
+    .min(5, { message: "5枚の画像が必要です。" }),
+  lastName: z.string(),
+  firstName: z.string(),
+  birthday: z.string(),
+  careLevel: z.enum([
+    "independence",
+    "needs_support_1",
+    "needs_support_2",
+    "needs_nursing_care_1",
+    "needs_nursing_care_2",
+    "needs_nursing_care_3",
+    "needs_nursing_care_4",
+    "needs_nursing_care_5",
+  ]),
+  groupId: z.string(),
+  gender: z.enum(["male", "female"]),
+  drugImages: z.array(z.custom<File>()),
+})
 
 export default function CreatePatientPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      faceImages: [],
+      lastName: "",
+      firstName: "",
+      birthday: "",
+      careLevel: undefined,
+      groupId: "",
+      gender: undefined,
+      drugImages: [],
+    },
+    resolver: zodResolver(formSchema),
+  })
+
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Create Patient
-        </h1>
-        <CreatePatientForm />
+    <section className="min-h-screen bg-[#F5F5F5] px-4 pb-8 pt-11">
+      <h2 className="text-[20px] text-[#C2B37F]">利用者情報</h2>
+      <div className="mt-4 rounded-2xl bg-white p-4">
+        <Form {...form}>
+          <form onSubmit={console.log} className="space-y-4">
+            <div className="flex items-center gap-1">
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem className="max-w-[150px] space-y-0">
+                    <FormLabel className="text-[11px]">名字</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    {form.formState.errors.lastName && (
+                      <FormDescription>
+                        {form.formState.errors.lastName.message}
+                      </FormDescription>
+                    )}
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem className="max-w-[150px] space-y-0">
+                    <FormLabel className="text-[11px]">名前</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    {form.formState.errors.firstName && (
+                      <FormDescription>
+                        {form.formState.errors.firstName.message}
+                      </FormDescription>
+                    )}
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormField
+              control={form.control}
+              name="birthday"
+              render={({ field }) => (
+                <FormItem className="max-w-[150px] space-y-0">
+                  <FormLabel className="text-[11px]">生年月日</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  {form.formState.errors.birthday && (
+                    <FormDescription>
+                      {form.formState.errors.birthday.message}
+                    </FormDescription>
+                  )}
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="careLevel"
+              render={({ field }) => (
+                <FormItem className="max-w-[150px] space-y-0">
+                  <FormLabel className="text-[11px]">介護度</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="independence">自立</SelectItem>
+                      <SelectItem value="needs_support_1">要支援1</SelectItem>
+                      <SelectItem value="needs_support_2">要支援2</SelectItem>
+                      <SelectItem value="needs_nursing_care_1">
+                        要介護1
+                      </SelectItem>
+                      <SelectItem value="needs_nursing_care_2">
+                        要介護2
+                      </SelectItem>
+                      <SelectItem value="needs_nursing_care_3">
+                        要介護3
+                      </SelectItem>
+                      <SelectItem value="needs_nursing_care_4">
+                        要介護4
+                      </SelectItem>
+                      <SelectItem value="needs_nursing_care_5">
+                        要介護5
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                  {form.formState.errors.careLevel && (
+                    <FormDescription>
+                      {form.formState.errors.careLevel.message}
+                    </FormDescription>
+                  )}
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="groupId"
+              render={({ field }) => (
+                <FormItem className="max-w-[150px] space-y-0">
+                  <FormLabel className="text-[11px]">グループ</FormLabel>
+                  <GroupsSelect
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    isError={!!form.formState.errors.groupId}
+                  />
+
+                  {form.formState.errors.groupId && (
+                    <FormDescription>
+                      {form.formState.errors.groupId.message}
+                    </FormDescription>
+                  )}
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="gender"
+              render={({ field }) => (
+                <FormItem className="max-w-[150px] space-y-0">
+                  <FormLabel className="text-[11px]">性別</FormLabel>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="male" />
+                        </FormControl>
+                        <FormLabel className="font-normal">男性</FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="female" />
+                        </FormControl>
+                        <FormLabel className="font-normal">女性</FormLabel>
+                      </FormItem>
+                    </RadioGroup>
+                  </FormControl>
+                  {form.formState.errors.gender && (
+                    <FormDescription>
+                      {form.formState.errors.gender.message}
+                    </FormDescription>
+                  )}
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
       </div>
     </section>
   )
