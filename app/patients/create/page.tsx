35d17d2eb52c7464d893b4e9c4f979101edc4f16
kotaro1574 <shake1574@gmail.com>
2024-06-08 15:51:10 -0700

    update: schema.tsを作成

--- app/patients/create/page.tsx
+++ app/patients/create/page.tsx
@@ -1,36 +1,12 @@
 "use client"
 
 import { PatientInfoFormField } from "@/feature/patient/patient-info-form-field"
+import { createPatientFormSchema } from "@/feature/patient/schema"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
 import { Form } from "@/components/ui/form"
-
-export const createPatientFormSchema = z.object({
-  faceImages: z
-    .array(z.custom<File>())
-    .min(5, { message: "5枚の画像が必要です。" }),
-  lastName: z.string(),
-  firstName: z.string(),
-  era: z.string(),
-  year: z.string(),
-  month: z.string(),
-  day: z.string(),
-  careLevel: z.enum([
-    "independence",
-    "needs_support_1",
-    "needs_support_2",
-    "needs_nursing_care_1",
-    "needs_nursing_care_2",
-    "needs_nursing_care_3",
-    "needs_nursing_care_4",
-    "needs_nursing_care_5",
-  ]),
-  groupId: z.string(),
-  gender: z.enum(["male", "female"]),
-  drugImages: z.array(z.custom<File>()),
-})
 
 export default function CreatePatientPage() {
   const form = useForm<z.infer<typeof createPatientFormSchema>>({
---
+++ feature/patient/schema.ts
@@ -0,0 +1,26 @@
+import { z } from "zod"
+
+export const createPatientFormSchema = z.object({
+  faceImages: z
+    .array(z.custom<File>())
+    .min(5, { message: "5枚の画像が必要です。" }),
+  lastName: z.string(),
+  firstName: z.string(),
+  era: z.string(),
+  year: z.string(),
+  month: z.string(),
+  day: z.string(),
+  careLevel: z.enum([
+    "independence",
+    "needs_support_1",
+    "needs_support_2",
+    "needs_nursing_care_1",
+    "needs_nursing_care_2",
+    "needs_nursing_care_3",
+    "needs_nursing_care_4",
+    "needs_nursing_care_5",
+  ]),
+  groupId: z.string(),
+  gender: z.enum(["male", "female"]),
+  drugImages: z.array(z.custom<File>()),
+})
