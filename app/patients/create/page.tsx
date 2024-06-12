3e567f36405c52b3df765b19da312989a3993149
kotaro1574 <shake1574@gmail.com>
2024-06-12 13:55:31 -0700

    update: ボタンを設置＆ボタンstyle調整

--- app/patients/create/page.tsx
+++ app/patients/create/page.tsx
@@ -6,6 +6,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
+import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 
 export default function CreatePatientPage() {
@@ -29,8 +30,24 @@ export default function CreatePatientPage() {
   return (
     <section className="min-h-screen bg-[#F5F5F5] px-4 pb-8 pt-11">
       <Form {...form}>
-        <form onSubmit={console.log}>
+        <form onSubmit={console.log} className="space-y-10">
           <PatientInfoFormField form={form} />
+          <div className="space-y-8">
+            <div className="flex gap-1">
+              <h2 className="text-[20px] text-[#C2B37F]">認証用人物写真</h2>
+              <p className="text-[10px] text-[#FF0000]">＊登録必須</p>
+            </div>
+            <Button
+              variant={"secondary"}
+              size={"secondary"}
+              className="block w-full"
+            >
+              顔を登録する
+            </Button>
+          </div>
+          <div>
+            <Button className="block w-full">登録</Button>
+          </div>
         </form>
       </Form>
     </section>
