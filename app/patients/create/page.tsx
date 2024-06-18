2530c7d65aebc45ad6c28ca5b813219af52d4fbd
kotaro1574 <shake1574@gmail.com>
2024-06-18 13:24:03 -0700

    update: face-images-formのコンポーネント化＆style調整

--- app/patients/create/page.tsx
+++ app/patients/create/page.tsx
@@ -1,20 +1,17 @@
 "use client"
 
 import { useState, useTransition } from "react"
-import Image from "next/image"
 import { useRouter } from "next/navigation"
 import { createPatient } from "@/actions/patients/create-patient"
-import { PatientFacesWebcamDialog } from "@/feature/patient/patient-faces-webcam-dialog"
+import { PatientFaceImagesFormField } from "@/feature/patient/patient-face-images-form-field"
 import { PatientInfoFormField } from "@/feature/patient/patient-info-form-field"
 import { createPatientFormSchema } from "@/feature/patient/schema"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
-import { AspectRatio } from "@/components/ui/aspect-ratio"
 import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
-import { Icons } from "@/components/ui/icons"
 import { useToast } from "@/components/ui/use-toast"
 
 export default function CreatePatientPage() {
@@ -87,48 +84,12 @@ export default function CreatePatientPage() {
     })
   }
 
-  const isFullFaceImages = form.watch("faceImages")?.length >= 5
-
   return (
     <section className="min-h-screen bg-[#F5F5F5] px-4 pb-8 pt-11">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-10">
           <PatientInfoFormField form={form} />
-          <div className="space-y-8">
-            <div className="flex gap-1">
-              <h2 className="text-[20px] text-[#C2B37F]">認証用人物写真</h2>
-              <p className="text-[10px] text-[#FF0000]">＊登録必須</p>
-            </div>
-            {isFullFaceImages && (
-              <div className="relative flex items-center justify-center">
-                <div className="relative w-full max-w-[150px]">
-                  <AspectRatio ratio={15 / 21}>
-                    <Image
-                      src={URL.createObjectURL(form.watch("faceImages")[0])}
-                      alt="face image"
-                      fill
-                      className="rounded-[8px] object-cover"
-                    />
-                  </AspectRatio>
-                  <div className="absolute right-[-12px] top-[-12px]">
-                    <Icons.faceImagesCheck />
-                  </div>
-                </div>
-              </div>
-            )}
-            <PatientFacesWebcamDialog
-              form={form}
-              trigger={
-                <Button
-                  variant="secondary"
-                  size="secondary"
-                  className="block w-full"
-                >
-                  {isFullFaceImages ? "顔を登録し直す" : "顔を登録する"}
-                </Button>
-              }
-            />
-          </div>
+          <PatientFaceImagesFormField form={form} />
           <div>
             <Button disabled={loading} className="block w-full">
               {loading ? "登録中..." : "登録する"}
