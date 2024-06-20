dcf7844477c4ee35b2278df6d9960389776a568d
nyny-sugar <nyny.sugar@gmail.com>
2024-06-20 14:06:19 -0700

    tab増やしてみた

--- app/patients/page.tsx
+++ app/patients/page.tsx
@@ -8,13 +8,15 @@ export default function PatientsPage() {
       <div className="mx-auto">
         <h2 className="mb-[46px] text-center text-[20px]">4/1(水)</h2>
         <Tabs defaultValue="all" className="">
-          <TabsList className="overflow-x-scroll rounded-b-lg px-4 shadow-shadow">
+          <TabsList className="space-x-2 overflow-x-scroll rounded-b-lg px-4 shadow-shadow">
             <TabsTrigger value="all">全て</TabsTrigger>
             <TabsTrigger value="floorA">Aフロア</TabsTrigger>
             <TabsTrigger value="floorB">Bフロア</TabsTrigger>
             <TabsTrigger value="floorC">Cフロア</TabsTrigger>
             <TabsTrigger value="floorD">Dフロア</TabsTrigger>
             <TabsTrigger value="floorE">Eフロア</TabsTrigger>
+            <TabsTrigger value="floorF">Fフロア</TabsTrigger>
+            <TabsTrigger value="floorG">Gフロア</TabsTrigger>
           </TabsList>
           <TabsContent value="all" className="bg-[#F5F5F5] px-4 py-8">
             Make changes to your account here.
@@ -32,6 +34,12 @@ export default function PatientsPage() {
             Make changes to your account here.
           </TabsContent>
           <TabsContent value="floorE" className="bg-[#F5F5F5] px-4 py-8">
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value="floorF" className="bg-[#F5F5F5] px-4 py-8">
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value="floorG" className="bg-[#F5F5F5] px-4 py-8">
             Make changes to your account here.
           </TabsContent>
         </Tabs>
