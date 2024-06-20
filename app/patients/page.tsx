21d6e7e4ed8f5f41da8b153c7c40a4178789d36c
nyny-sugar <nyny.sugar@gmail.com>
2024-06-20 15:40:21 -0700

    背景色の設定

--- app/patients/page.tsx
+++ app/patients/page.tsx
@@ -4,11 +4,11 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function PatientsPage() {
   return (
-    <section className="pt-[52px]">
+    <section className="min-h-screen bg-[#F5F5F5] pt-[52px]">
       <div className="mx-auto">
-        <h2 className="mb-[46px] text-center text-[20px]">4/1(水)</h2>
+        <h2 className="bg-white pb-[46px] text-center text-[20px]">4/1(水)</h2>
         <Tabs defaultValue="all" className="">
-          <TabsList className="space-x-2 overflow-x-scroll rounded-b-lg px-4 shadow-shadow">
+          <TabsList className="space-x-2 overflow-x-scroll rounded-b-lg bg-white px-4 shadow-shadow">
             <TabsTrigger value="all">全て</TabsTrigger>
             <TabsTrigger value="floorA">
               <p className="line-clamp-1">Aフロア</p>
@@ -32,28 +32,28 @@ export default function PatientsPage() {
               <p className="line-clamp-1">Gフロア</p>
             </TabsTrigger>
           </TabsList>
-          <TabsContent value="all" className="bg-[#F5F5F5] px-4 py-8">
+          <TabsContent value="all" className="px-4 py-8">
             Make changes to your account here.
           </TabsContent>
-          <TabsContent value="floorA" className="bg-[#F5F5F5] px-4 py-8">
+          <TabsContent value="floorA" className="px-4 py-8">
             Make changes to your account here.
           </TabsContent>
-          <TabsContent value="floorB" className="bg-[#F5F5F5] px-4 py-8">
+          <TabsContent value="floorB" className="px-4 py-8">
             Make changes to your account here.
           </TabsContent>
-          <TabsContent value="floorC" className="bg-[#F5F5F5] px-4 py-8">
+          <TabsContent value="floorC" className="px-4 py-8">
             Make changes to your account here.
           </TabsContent>
-          <TabsContent value="floorD" className="bg-[#F5F5F5] px-4 py-8">
+          <TabsContent value="floorD" className="px-4 py-8">
             Make changes to your account here.
           </TabsContent>
-          <TabsContent value="floorE" className="bg-[#F5F5F5] px-4 py-8">
+          <TabsContent value="floorE" className="px-4 py-8">
             Make changes to your account here.
           </TabsContent>
-          <TabsContent value="floorF" className="bg-[#F5F5F5] px-4 py-8">
+          <TabsContent value="floorF" className="px-4 py-8">
             Make changes to your account here.
           </TabsContent>
-          <TabsContent value="floorG" className="bg-[#F5F5F5] px-4 py-8">
+          <TabsContent value="floorG" className="px-4 py-8">
             Make changes to your account here.
           </TabsContent>
         </Tabs>
