f7ab0800395aa57b32781d5f7a58b5cd08aa072f
nyny-sugar <nyny.sugar@gmail.com>
2024-06-24 15:23:33 -0700

    データ取得

--- app/patients/page.tsx
+++ app/patients/page.tsx
@@ -1,99 +1,72 @@
+import { error } from "console"
+import { redirect } from "next/dist/server/api-utils"
 import { GroupTabs } from "@/feature/group/group-tabs"
 
+import { createClient } from "@/lib/supabase/server"
 import { formatDate } from "@/lib/utils"
 
-const tabs = [
-  {
-    value: "all",
-    contents: [
-      {
-        name: "山田 花子",
-      },
-      {
-        name: "山田 太郎",
-      },
-    ],
-  },
-  {
-    value: "floorA",
-    contents: [
-      {
-        name: "山田 花子",
-      },
-      {
-        name: "山田 太郎",
-      },
-    ],
-  },
-  {
-    value: "floorB",
-    contents: [
-      {
-        name: "山田 花子",
-      },
-      {
-        name: "山田 太郎",
-      },
-    ],
-  },
-  {
-    value: "floorC",
-    contents: [
-      {
-        name: "山田 花子",
-      },
-      {
-        name: "山田 太郎",
-      },
-    ],
-  },
-  {
-    value: "floorD",
-    contents: [
-      {
-        name: "山田 花子",
-      },
-      {
-        name: "山田 太郎",
-      },
-    ],
-  },
-  {
-    value: "floorE",
-    contents: [
-      {
-        name: "山田 花子",
-      },
-      {
-        name: "山田 太郎",
-      },
-    ],
-  },
-  {
-    value: "floorF",
-    contents: [
-      {
-        name: "山田 花子",
-      },
-      {
-        name: "山田 太郎",
-      },
-    ],
-  },
-  {
-    value: "floorG",
-    contents: [
-      {
-        name: "山田 花子",
-      },
-      {
-        name: "山田 太郎",
-      },
-    ],
-  },
-]
+export default async function PatientsPage() {
+  const supabase = createClient()
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser()
 
-export default function PatientsPage() {
+  if (!user) {
+    return { redirect: { destination: "/login", permanent: false } }
+  }
+
+  const { data: profile, error: profileError } = await supabase
+    .from("profiles")
+    .select("facility_id")
+    .eq("id", user.id)
+    .single()
+
+  if (profileError) {
+    return <div>error</div>
+  }
+
+  const { data: groups, error: groupsError } = await supabase
+    .from("groups")
+    .select("*")
+    .eq("facility_id", profile.facility_id)
+
+  if (groupsError) {
+    return <div>error</div>
+  }
+
+  const { data: patients, error: patientsError } = await supabase
+    .from("patients")
+    .select("first_name, last_name, group_id")
+    .eq("facility_id", profile.facility_id)
+
+  if (patientsError) {
+    return <div>error</div>
+  }
+
+  const _tabs = groups.map((group) => ({
+    value: group.name,
+    contents: patients
+      .filter((patient) => patient.group_id === group.id)
+      .map((patient) => {
+        return {
+          name: `${patient.last_name} ${patient.first_name}`,
+        }
+      }),
+  }))
+
+  const tabs = [
+    {
+      value: "all",
+      contents: patients.map((patient) => {
+        return {
+          name: `${patient.last_name} ${patient.first_name}`,
+        }
+      }),
+    },
+    ..._tabs,
+  ]
+
   const today = new Date()
   return (
     <section className="min-h-screen bg-[#F5F5F5] pt-[44px]">
