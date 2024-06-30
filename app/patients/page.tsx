220073ddbac12e16bfbcd87ad0ede99babaffb32
nyny-sugar <nyny.sugar@gmail.com>
2024-06-30 15:39:17 -0700

    余白を修正

--- app/patients/page.tsx
+++ app/patients/page.tsx
@@ -83,7 +83,7 @@ export default async function PatientsPage() {
   return (
     <section className="min-h-screen bg-[#F5F5F5]">
       <div className="mx-auto">
-        <h2 className="mt-[44px] bg-white pb-[46px] pt-[16px] text-center text-[20px]">
+        <h2 className="bg-white pb-[46px] pt-[60px] text-center text-[20px]">
           {formatDate(today, "M/d(EEE)")}
         </h2>
         <GroupTabs items={tabs} />
