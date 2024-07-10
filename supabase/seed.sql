2d5a66fb0065e3f956fdddf26ecc2ed5e07c93a7
kotaro1574 <shake1574@gmail.com>
2024-07-10 15:20:47 -0700

    update: seedData

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -11,4 +11,4 @@ INSERT INTO groups (name, facility_id) VALUES
 ('groupB', (SELECT id FROM facilities WHERE name = 'リハビリテーション施設'));
 
 INSERT INTO patients (last_name, first_name, birthday, care_level, facility_id, group_id, gender, image_id, face_ids) VALUES
-('杉田', '浩太朗', 'H5.11.2生(30歳)', 'independence', (SELECT id FROM facilities WHERE name = '健康センター'), (SELECT id FROM groups WHERE name = 'グループA'), 'male', '76de6609-3fcb-4dcf-8b55-7cdf4a729724', array['c6993c3a-d03e-4d2f-b48b-ee56827080c0', 'b77c2537-0eed-4702-97c3-55ecd6fe5389', '95e5328e-3e53-4142-85a1-a818ca7596aa', '697c867c-0796-4d41-9f08-d56d30c771a9', '1e65d962-fc5f-49fd-9481-0bf691197fef']);
+('杉田', '浩太朗', 'H5.11.2生(30歳)', 'independence', (SELECT id FROM facilities WHERE name = '健康センター'), (SELECT id FROM groups WHERE name = 'グループA'), 'male', '49d53b48-08f5-479a-bc08-6e8a7f764a95', array['f7048c9d-4497-4f49-8123-d82cdf50a0a7', '3bcd163c-3100-4c0a-99d3-369a2b52a17', 'fe0a8aee-3d45-42bf-a7de-aa424916aef4', '3be0a94d-3eb0-409d-9c97-f354ff573358', '21720c1-efdf-4498-832d-f1b84b32c1bf']);
