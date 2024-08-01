1db07865a68fa363c67c90503dc0399f46de4bb0
kotaro1574 <shake1574@gmail.com>
2024-08-01 16:01:12 -0700

    update: alertフォームのstyle調整

--- feature/patient/createForm/field/alertField/patient-alert-form-field.tsx
+++ feature/patient/createForm/field/alertField/patient-alert-form-field.tsx
@@ -1,3 +1,5 @@
+import { HourSelect } from "@/feature/patient/hour-select"
+import { MinuteSelect } from "@/feature/patient/minute-select"
 import { UseFormReturn, useFieldArray } from "react-hook-form"
 import { z } from "zod"
 
@@ -9,6 +11,7 @@ import {
   FormLabel,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
 import { Switch } from "@/components/ui/switch"
 
 import { createPatientFormSchema } from "../../schema"
@@ -31,83 +34,100 @@ export function PatientAlertFormField({
           key={field.id}
           className="mt-4 space-y-4 rounded-2xl bg-white p-4 shadow-shadow"
         >
-          <div className="">
-            <div className="flex items-center justify-between">
-              <p className="text-xl text-[#A4A4A4]">{`アラートタイマー${
-                index + 1
-              }`}</p>
+          <div className="flex items-center justify-between">
+            <p className="text-xl text-[#A4A4A4]">{`アラートタイマー${
+              index + 1
+            }`}</p>
+            <FormField
+              control={form.control}
+              name={`alert.${index}.isAlertEnabled`}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="mt-6 flex items-center justify-between border-b-[0.5px] border-[#A4A4A4] px-1 py-2">
+            <FormLabel className="w-full text-[11px]">時間</FormLabel>
+            <div className="flex w-auto flex-1 items-center gap-2">
               <FormField
                 control={form.control}
-                name={`alert.${index}.isAlertEnabled`}
+                name={`alert.${index}.hour`}
                 render={({ field }) => (
-                  <FormItem>
+                  <FormItem className="w-[80px] space-y-0">
                     <FormControl>
-                      <Switch
-                        checked={field.value}
-                        onCheckedChange={field.onChange}
+                      <HourSelect
+                        onValueChange={field.onChange}
+                        defaultValue={String(field.value)}
+                        isError={!!form.formState.errors?.alert?.[index]?.hour}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <div>:</div>
+              <FormField
+                control={form.control}
+                name={`alert.${index}.minute`}
+                render={({ field }) => (
+                  <FormItem className="w-[80px] space-y-0">
+                    <FormControl>
+                      <MinuteSelect
+                        onValueChange={field.onChange}
+                        defaultValue={String(field.value)}
+                        isError={
+                          !!form.formState.errors?.alert?.[index]?.minute
+                        }
                       />
                     </FormControl>
                   </FormItem>
                 )}
               />
             </div>
-            <FormField
-              control={form.control}
-              name={`alert.${index}.hour`}
-              render={({ field }) => (
-                <>
-                  <FormItem className="max-w-[200px] space-y-0">
-                    <FormLabel className="text-[11px]">時間</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                  </FormItem>
-                </>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name={`alert.${index}.minute`}
-              render={({ field }) => (
-                <>
-                  <FormItem className="max-w-[200px] space-y-0">
-                    <FormLabel className="text-[11px]">分</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                  </FormItem>
-                </>
-              )}
-            />
+          </div>
+          <div className="flex items-center justify-between border-b-[0.5px] border-[#A4A4A4]  px-1 py-2">
+            <FormLabel className="text-[11px]">繰り返し設定</FormLabel>
             <FormField
               control={form.control}
               name={`alert.${index}.repeatStetting`}
               render={({ field }) => (
-                <>
-                  <FormItem className="max-w-[200px] space-y-0">
-                    <FormLabel className="text-[11px]">繰り返し設定</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                  </FormItem>
-                </>
+                <FormItem className="max-w-[200px] space-y-0">
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
               )}
             />
+          </div>
+          <div className="flex items-center justify-between border-b-[0.5px] border-[#A4A4A4] px-1 py-2">
+            <FormLabel className="text-[11px]">日付</FormLabel>
             <FormField
               control={form.control}
               name={`alert.${index}.date`}
               render={({ field }) => (
-                <>
-                  <FormItem className="max-w-[200px] space-y-0">
-                    <FormLabel className="text-[11px]">日</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                  </FormItem>
-                </>
+                <FormItem className="max-w-[200px] space-y-0">
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
               )}
             />
           </div>
+
+          <Button
+            className="mt-4 w-full max-w-[100px]"
+            variant="secondary"
+            size="secondary"
+            type="button"
+          >
+            削除
+          </Button>
         </div>
       ))}
       <Button
