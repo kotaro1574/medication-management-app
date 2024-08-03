df1fba2bc102d3d2296239cae00ab6dec71877d0
kotaro1574 <shake1574@gmail.com>
2024-08-03 13:27:45 -0700

    fix: 不要import

--- feature/patient/createForm/field/alertField/patient-alert-form-field.tsx
+++ feature/patient/createForm/field/alertField/patient-alert-form-field.tsx
@@ -2,11 +2,9 @@ import { HourSelect } from "@/feature/patient/hour-select"
 import { MinuteSelect } from "@/feature/patient/minute-select"
 import { RepeatStettingSelect } from "@/feature/patient/repeat-stetting-select"
 import { format } from "date-fns"
-import { CalendarIcon } from "lucide-react"
 import { UseFormReturn, useFieldArray } from "react-hook-form"
 import { z } from "zod"
 
-import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
 import {
