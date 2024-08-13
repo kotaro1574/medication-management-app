b6ff3e285977198f08ae8c88326261d3af5990e9
kotaro1574 <shake1574@gmail.com>
2024-08-13 14:22:21 -0700

    update: メールアドレス変更の仕様が変わったので修正

---
+++ feature/user/email-change-confirm-toast.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { useSearchParams } from "next/navigation"
+import { setLoginInfo } from "@/actions/cookie/set-login-info"
+import { User } from "@supabase/supabase-js"
+
+import { useToast } from "@/components/ui/use-toast"
+
+type Props = {
+  user: User
+  name: string
+}
+
+export function EmailChangeConfirmToast({ user, name }: Props) {
+  const searchParams = useSearchParams()
+  const code = searchParams.get("code")
+  const [mounted, setMounted] = useState(false)
+  const { toast } = useToast()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  useEffect(() => {
+    if (code && mounted && user.email) {
+      toast({ title: "メールアドレスを更新しました" })
+      setLoginInfo({
+        id: user.id,
+        name,
+        email: user.email,
+      })
+    }
+  }, [code, mounted, name, toast, user.email, user.id])
+
+  return null
+}
