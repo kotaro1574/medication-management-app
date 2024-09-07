46de1ef871b293af225fba52c437659abc9ac991
kotaro1574 <shake1574@gmail.com>
2024-09-07 14:53:40 -0700

    delete

--- feature/auth/face-login-camera-dialog.tsx
+++ feature/auth/face-login-camera-dialog.tsx
@@ -46,8 +46,6 @@ export function FaceLoginCameraDialog({ trigger }: Props) {
         imageSrc: base64Data,
       })
 
-      console.log(response)
-
       if (response.success) {
         const { accessToken, refreshToken } = await generateCustomToken(
           response.id
