3f478634b4d495f660b88463e5cbf4f15a58b59a
kotaro1574 <shake1574@gmail.com>
2024-10-07 13:41:52 +0900

    update: facilitiesテーブルをupdate

---
+++ supabase/migrations/20241007042316_name_en_and_plan_columns_to_facilities_table.sql
@@ -0,0 +1,14 @@
+-- enum型を作成
+do $$ begin
+    if not exists (select 1 from pg_type where typname = 'plan_type') then
+        create type plan_type as enum ('松', '竹', '梅');
+    end if;
+end $$;
+
+-- facilitiesテーブルにplanカラムを追加
+alter table public.facilities
+    rename column name to name_ja;
+
+alter table public.facilities
+    add column name_en varchar(255) not null,
+    add column plan plan_type not null;
--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,11 +1,11 @@
 -- facilitiesテーブルにデータを挿入
-INSERT INTO facilities (name, email) VALUES
-('健康センター', 'facility1@example.com'),
-('リハビリテーション施設', 'facility2@example.com');
+INSERT INTO facilities (name_ja, name_en, email, plan) VALUES
+('健康センター', 'health_center', 'facility1@example.com', '竹'),
+('リハビリテーション施設', 'rehabilitation_facilities', 'facility2@example.com', '梅');
 
 -- groupsテーブルにデータを挿入
 INSERT INTO groups (name, facility_id) VALUES
-('グループA', (SELECT id FROM facilities WHERE name = '健康センター')),
-('グループB', (SELECT id FROM facilities WHERE name = '健康センター')),
-('groupA', (SELECT id FROM facilities WHERE name = 'リハビリテーション施設')),
-('groupB', (SELECT id FROM facilities WHERE name = 'リハビリテーション施設'));
+('グループA', (SELECT id FROM facilities WHERE name_ja = '健康センター')),
+('グループB', (SELECT id FROM facilities WHERE name_ja = '健康センター')),
+('groupA', (SELECT id FROM facilities WHERE name_ja = 'リハビリテーション施設')),
+('groupB', (SELECT id FROM facilities WHERE name_ja = 'リハビリテーション施設'));
