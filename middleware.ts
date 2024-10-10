ab40691a5434b7571d1eccc5ef2749336c019f98
kotaro1574 <shake1574@gmail.com>
2024-10-10 13:54:20 +0900

    fix

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@ import { type NextRequest } from "next/server"
 import { updateSession } from "./lib/supabase/middleware"
 
 export async function middleware(request: NextRequest) {
-  // update user's auth session
   return await updateSession(request)
 }
 
