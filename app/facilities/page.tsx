4b951aba65b250087fe818cc6a0017e8e0d68381
kotaro1574 <shake1574@gmail.com>
2024-10-11 13:13:55 +0900

    create: 施設追加！！

--- app/facilities/page.tsx
+++ app/facilities/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 
 import { createClient } from "@/lib/supabase/server"
+import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/ui/icons"
 
 export default async function FacilitiesPage() {
@@ -17,35 +18,45 @@ export default async function FacilitiesPage() {
   return (
     <section className="px-4 pb-8 pt-[62px]">
       <h2 className=" text-xl text-[#C2B37F]">施設一覧</h2>
-      {facilities.length === 0 ? (
-        <p className="mt-4 text-sm text-[#A4A4A4]">
-          施設が登録されていません。
-        </p>
-      ) : (
-        <div className="mt-[60px]">
-          <h3 className="text-base">施設名</h3>
-          <div className="pt-[34px]">
-            {facilities.map((facility) => (
-              <div
-                key={facility.id}
-                className="mb-[26px] flex items-center justify-between border-b-[0.5px] border-[#A4A4A4] pb-[14px]"
-              >
-                <div className="max-w-60 sm:max-w-[500px]">
-                  <p className="whitespace-normal break-words">
-                    {facility.name_jp}
-                  </p>
+      <div>
+        {facilities.length === 0 ? (
+          <p className="mt-4 text-sm text-[#A4A4A4]">
+            施設が登録されていません。
+          </p>
+        ) : (
+          <div className="mt-[60px]">
+            <h3 className="text-base">施設名</h3>
+            <div className="pt-[34px]">
+              {facilities.map((facility) => (
+                <div
+                  key={facility.id}
+                  className="mb-[26px] flex items-center justify-between border-b-[0.5px] border-[#A4A4A4] pb-[14px]"
+                >
+                  <div className="max-w-60 sm:max-w-[500px]">
+                    <p className="whitespace-normal break-words">
+                      {facility.name_jp}
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-4">
+                    <Link href={`/facilities/${facility.id}`}>
+                      <Icons.edit />
+                    </Link>
+                    <Icons.trash className="size-6" />
+                  </div>
                 </div>
-                <div className="flex items-center gap-4">
-                  <Link href={`/facilities/${facility.id}`}>
-                    <Icons.edit />
-                  </Link>
-                  <Icons.trash className="size-6" />
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
+      <Link
+        className={`${buttonVariants()} ${
+          facilities.length === 0 ? "mt-4" : "mt-[6px]"
+        } block w-full`}
+        href="/facilities/create"
+      >
+        追加
+      </Link>
     </section>
   )
 }
